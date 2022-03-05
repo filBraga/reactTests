f4649db675ad3e353db8340d6d1e969aad8e285b
Filipe <filipe.b.braga@gmail.com>
2022-03-04 23:26:09 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,24 @@
-test('TesteCompleto', () => {
-  it('TesteInterno', () => {
-    expect(1).toBe(1);
-  });
-  it('TesteInterno', () => {
-    expect(1).toBe(1);
-  });
-  it('TesteInterno', () => {
-    expect(1).toBe(1);
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found,'
+    + 'se a pessoa não tiver pokémons favoritos.', async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    // valores entre / algumaCoisa /i ignoram letras maiúsculas ou minúsculas
+    const aboutLink = screen.getByText(/Favorite Pokémons/i);
+    expect(aboutLink).toBeDefined();
+
+    userEvent.click(aboutLink);
+
+    const teste = await screen.findByText('No favorite pokemon found');
+    expect(teste).toBeDefined();
   });
 });
